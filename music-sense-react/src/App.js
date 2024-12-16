2e91a8c92be9ee6f0cf47cf9a66d36a0500b986e
Davor Kuharić <davorkuharic1997@gmail.com>
2024-12-16 20:20:37 +0100

    Starting of HTML and CSS

--- music-sense-react/src/App.js
+++ music-sense-react/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import MusicSenseLogo from "./assets/MusicSenseLogo.svg";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faRepeat,
+  faForwardStep,
+  faShuffle,
+  faStar,
+  faBars,
+  faXmark,
+  faUser,
+  faMusic,
+  faHome,
+} from "@fortawesome/free-solid-svg-icons";
+import { faStar as faRegStar } from "@fortawesome/free-regular-svg-icons"; //ista ikonica drugaciji stil, pozivat cu je sa faRegStar
+
+// Ovako prikazujem fontawesome ikonice <FontAwesomeIcon icon={faPlay} className="playIcon" />
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div className="wrapper">
+        <nav>
+          <img src={MusicSenseLogo} id="logoStyle" alt="logo" />
+          <FontAwesomeIcon icon={faBars} className="hamburgerMenu" />
+        </nav>
+        <section className="nowPlayingQueue">
+          <article className="nowPlayingSection">
+            <h1 className="title">Now playing</h1>
+          </article>
+          <article className="inQueueSection">
+            <h1 className="title">In queue</h1>
+          </article>
+        </section>
+        <section className="topPlaylists">
+          <h1 className="title">Top playlists</h1>
+        </section>
+        {/* <footer>
+        </footer> */}
+      </div>
+    </main>
   );
 }
 
