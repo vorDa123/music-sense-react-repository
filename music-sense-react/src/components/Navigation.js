dc48e719219b838444846b58401d444c4eba9dbc
Davor Kuharić <davorkuharic1997@gmail.com>
2024-12-28 21:18:33 +0100

    Trying to connect to spotify API

--- music-sense-react/src/components/Navigation.js
+++ music-sense-react/src/components/Navigation.js
@@ -1,19 +1,30 @@
 import "App.css";
 import MusicSenseLogo from "assets/MusicSenseLogo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBars,
-  faXmark,
-  faUser,
-  faMusic,
-  faHome,
-} from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+//Components
+import SignInRegister from "components/SignInRegister.js";
 
 export default function Navigation() {
+  let [menuClicked, setMenuClicked] = useState(false);
+
+  let handleSidebarMenu = () => {
+    setMenuClicked((prevState) => !prevState);
+  };
+
   return (
-    <nav>
-      <img src={MusicSenseLogo} id="logoStyle" alt="logo" />
-      <FontAwesomeIcon icon={faBars} className="hamburgerMenu" />
-    </nav>
+    <>
+      <nav>
+        <img src={MusicSenseLogo} id="logoStyle" alt="logo" />
+        <FontAwesomeIcon
+          icon={faBars}
+          className="hamburgerMenu"
+          onClick={handleSidebarMenu}
+        />
+      </nav>
+      {menuClicked ? <SignInRegister /> : null}
+    </>
   );
 }
--- music-sense-react/src/components/SignInRegister.js
+++ music-sense-react/src/components/SignInRegister.js
@@ -2,22 +2,61 @@ import "App.css";
 import MusicSenseLogo from "assets/MusicSenseLogo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 export default function SignInRegister() {
-  return (
-    <section className="signInRegister">
-      <div className="titleAndIcon">
-        <h1 className="title">Menu</h1>
-        <FontAwesomeIcon icon={faXmark} className="hamburgerMenu" />
-      </div>
-      <div className="buttons">
-        <button>Sign in</button>
-        <br />
-        <button>Register</button>
-        <p>OR</p>
-        <button>Sign in with Spotify</button>
-      </div>
-      <img src={MusicSenseLogo} alt="logo" width={150} className="paddingBottom"/>
-    </section>
-  );
+  const CLIENT_ID = "727a7c097cfb4ec4997132e3824c3a6d";
+  const CLIENT_SECRET = "96548f701f594b1b980d1d33ef2fd04b";
+
+  useEffect(() => {
+    // API Access Token
+    let authParameters = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+    };
+    fetch("https://accounts.spotify.com/api/token", authParameters)
+      .then((result) => result.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  let [menuClicked, setMenuClicked] = useState(true);
+
+  let handleSidebarMenu = () => {
+    setMenuClicked((prevState) => !prevState);
+  };
+
+  if (menuClicked) {
+    return (
+      <>
+        <section className="signInRegister">
+          <div className="titleAndIcon">
+            <h1 className="title">Menu</h1>
+            <FontAwesomeIcon
+              icon={faXmark}
+              className="hamburgerMenu"
+              onClick={handleSidebarMenu}
+            />
+          </div>
+          <div className="buttons">
+            <button>Sign in</button>
+            <br />
+            <button>Register</button>
+            <p>OR</p>
+            <button>Sign in with Spotify</button>
+          </div>
+          <img
+            src={MusicSenseLogo}
+            alt="logo"
+            width={150}
+            className="paddingBottom"
+          />
+        </section>
+      </>
+    );
+  } else {
+    return <></>;
+  }
 }
