435fa195840402e82097ffcc1b77b6f97cb9c441
Davor Kuharić <davorkuharic1997@gmail.com>
2025-01-19 15:55:53 +0100

    Playlist card tilt animation

--- music-sense-react/src/components/Playlist.js
+++ music-sense-react/src/components/Playlist.js
@@ -1,16 +1,65 @@
 import "App.css";
 import { usePlaylist } from "../PlaylistProvider";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useMotionTemplate,
+  useSpring,
+} from "motion/react";
+import { useEffect, useState, useRef } from "react";
 
 export default function Playlist({ playlistId, playlistName, playlistImage }) {
   const { setPlaylistId } = usePlaylist();
   const handleCardClick = () => {
     setPlaylistId(playlistId);
   };
+
+  const xCoord = useMotionValue(0);
+  const yCoord = useMotionValue(0);
+
+  const mouseXSpring = useSpring(xCoord);
+  const mouseYSpring = useSpring(yCoord);
+
+  const rotateX = useTransform(
+    mouseYSpring,
+    [-0.5, 0.5],
+    ["17.5deg", "-17.5deg"]
+  );
+
+  const rotateY = useTransform(
+    mouseXSpring,
+    [-0.5, 0.5],
+    ["-17.5deg", "17.5deg"]
+  );
+
+  const handleMouseMove = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    const xPercentage = mouseX / width - 0.5;
+    const yPercentage = mouseY / height - 0.5;
+
+    xCoord.set(xPercentage);
+    yCoord.set(yPercentage);
+  };
+
+  const handleMouseLeave = () => {
+    xCoord.set(0);
+    yCoord.set(0);
+  };
+
   return (
-    <div
+    <motion.div
       className="playlist"
       key={playlistId}
       style={{
+        rotateX,
+        rotateY,
         backgroundImage: `url( ${playlistImage} )`,
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
@@ -18,6 +67,8 @@ export default function Playlist({ playlistId, playlistName, playlistImage }) {
         cursor: "pointer",
       }}
       onClick={handleCardClick}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
     >
       <div
         style={{
@@ -29,6 +80,6 @@ export default function Playlist({ playlistId, playlistName, playlistImage }) {
       >
         <span className="playlistName">{playlistName}</span>
       </div>
-    </div>
+    </motion.div>
   );
 }
